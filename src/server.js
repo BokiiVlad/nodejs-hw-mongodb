15a9ad4fff7ec792501f57119b36926b5487799e
Vlad Bokii <bokiyvladik10@gmail.com>
2025-05-11 00:48:47 +0300

    hw

--- src/server.js
+++ src/server.js
@@ -3,13 +3,15 @@ import pino from "pino-http";
 import cors from "cors";
 import dotenv from "dotenv";
 import { getEnvVar } from "./utils/getEnvVar.js";
+import { getAllContacts, getContactById } from "./services/contacts.js";
+
 
 export const setupServer = async () => {
     dotenv.config();
     const app = express();
     const PORT = Number(getEnvVar("PORT", "3000"));
 
-
+    app.use(express.json());
     app.use(pino({
         transport: {
             target: 'pino-pretty',
@@ -17,6 +19,32 @@ export const setupServer = async () => {
     }),);
 
     app.use(cors());
+
+    app.get("/contacts", async (req, res, next) => {
+        const contacts = await getAllContacts();
+        res.status(200).json({
+            status: 200,
+            message: "Successfully found contacts!",
+            data: contacts,
+        });
+    });
+
+    app.get("/contacts/:contactId", (req, res, next) => {
+        const { contactId } = req.params;
+        const contact = getContactById(contactId);
+        if (!contact) {
+            res.status(404).json({
+                message: 'Contact not found',
+            });
+            return;
+        };
+        res.status(200).json({
+            status: 200,
+            message: "Successfully found contact with id {contactId}!",
+            data: contact,
+        }
+        );
+    });
 
     app.listen(PORT, (error) => {
         if (error) {
