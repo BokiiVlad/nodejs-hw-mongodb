c5def5219dbc6c51396cda838f710a154948a90d
Vlad Bokii <bokiyvladik10@gmail.com>
2025-05-15 17:06:30 +0300

    hw

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,4 +1,5 @@
 import { getAllContacts, getContactById } from "../services/contacts.js";
+import createHttpError from "http-errors";
 
 export const getAllContactsController = async (req, res) => {
     const contacts = await getAllContacts();
@@ -13,11 +14,10 @@ export const getContactByIdController = async (req, res) => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
     if (!contact) {
-        res.status(404).json({
-            message: 'Contact not found',
-        });
-        return;
+        throw createHttpError(404, "Contact not found");
+
     };
+
     res.status(200).json({
         status: 200,
         message: `Successfully found contact with id ${contactId}!`,
